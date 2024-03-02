cc13435e99be0dc5a9daa0020f1ae22c8d857a0d
3hrsforsleep <rbznrzy@gmail.com>
2024-03-02 18:34:08 +0500

    hw_2.2.4

--- calculations.js
+++ calculations.js
@@ -23,3 +23,37 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
+
+
+/** Операция инкремента с префиксной нотацией */
+let incrementedNumberPrefix = function incrementExample(number) {
+  let x = ++number
+  return x;
+}
+
+console.log(incrementedNumberPrefix(16));
+
+/** Операция инкремента с постфиксной нотацией */
+let incrementedNumberPostfix = function incrementExample(number) {
+  number++
+  return number;
+}
+
+console.log(incrementedNumberPostfix(16));
+
+/** Операция декремента с префиксной нотацией */
+let decrementedNumberPrefix = function decrementExample(number) {
+  let x = --number
+  return x;
+}
+
+console.log(decrementedNumberPrefix(16));
+
+/** Операция декремента с постфиксной нотацией */
+let decrementedNumberPostfix = function decrementExample(number) {
+  number--
+  return number;
+}
+
+console.log(decrementedNumberPostfix(16));
+
